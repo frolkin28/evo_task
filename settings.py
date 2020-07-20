26704e2550840d12e8a385003b71b27f0dccfc3a
frolkin28 <frolkin2801@gmail.com>
2020-07-20 18:54:24 +0300

    remove print in the settings

--- settings.py
+++ settings.py
@@ -12,5 +12,3 @@ class Config:
     ENV = 'development'
     SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-print(BASE_DIR)
